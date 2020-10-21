f86865b5ff82e771cb51faba9750d46a6c45dd17
anselbrandt <ansel@anselbrandt.com>
2020-10-21 14:28:10 -0400

    Add prod wss and http address

--- web/src/utils/withApollo.ts
+++ web/src/utils/withApollo.ts
@@ -5,10 +5,10 @@ import { NextPageContext } from "next";
 import { getMainDefinition } from "@apollo/client/utilities";
 import { WebSocketLink } from "@apollo/client/link/ws";
 
-const wsLink:any = process.browser
+const wsLink: any = process.browser
   ? new WebSocketLink({
       // if you instantiate in the server, the error will be thrown
-      uri: `ws://localhost:4000/graphql`,
+      uri: process.env.WEBSOCKET || `ws://localhost:4000/graphql`,
       options: {
         reconnect: true,
       },
@@ -16,7 +16,7 @@ const wsLink:any = process.browser
   : null;
 
 const httplink = new HttpLink({
-  uri: "http://localhost:4000/graphql",
+  uri: process.env.HTTP || "http://localhost:4000/graphql",
 });
 
 const link = process.browser
